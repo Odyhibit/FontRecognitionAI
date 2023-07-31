a8321384303fe81bd6ff5ac2b6a3ba2e1f03e3c7
Odyhibit <josh.2990@gmail.com>
2023-07-30 21:42:54 -0500

    initial working version

---
+++ gui.py
@@ -0,0 +1,53 @@
+import tkinter.filedialog
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk, ImageOps
+
+def pick_cover(event: Event = Event()):
+    file_name = tkinter.filedialog.askopenfilename(initialdir="./test_images", filetypes=[("image files", ".jpg")])
+    if not file_name:
+        return
+    image_path_str.set(file_name)
+
+
+root = Tk()
+root.title("Font Recognizer")
+root.geometry("700x400")
+
+# notebook settings
+notebook = ttk.Notebook(root)
+notebook.pack(expand=True, fill=BOTH)
+
+# main screen variables
+image_path_str = StringVar()
+image_path_str.set("Font Image")
+
+# main tab widgets
+main_screen = Frame(notebook)
+main_screen.columnconfigure(0, minsize=400)
+main_screen.columnconfigure(1, minsize=200)
+# row 0
+file_lbl = ttk.Label(main_screen, textvariable=image_path_str, wraplength=400)
+file_lbl.grid(column=0, row=0, padx=4, pady=6)
+file_btn = Button(main_screen, text="Choose Image", command=pick_cover)
+file_btn.grid(column=1, row=0, padx=4, pady=6)
+# row 1
+preview_lbl = Label(main_screen, bd=2, relief="groove", width=36, height=16)
+preview_lbl.grid(column=0, row=1, rowspan=2, sticky="S", padx=4, pady=6)
+output_lbl = Label(main_screen, text="Font   Certainty")
+output_lbl.grid(column=1, row=1, sticky="S")
+output = Label(main_screen, bd=2, relief="groove", width=16, height=8)
+output.grid(column=1, row=2, sticky="N")
+
+# dashboard tab widgets
+dashboard_screen = Frame(notebook)
+dashboard_screen.columnconfigure(0, minsize=100)
+dashboard_screen.columnconfigure(1, minsize=400)
+dashboard_screen.columnconfigure(2, minsize=100)
+
+
+# notebook tabs
+notebook.add(main_screen, text="Main")
+notebook.add(dashboard_screen, text="Dashboard")
+
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from tensorflow import keras as keras
+
+
+def predict_from_image(test_image: np.array, this_model: keras.models) -> np.array('float'):
+    return this_model.predict(test_image)
+
+
+def load_image(image_file: str):
+    img_array = keras.utils.load_img(image_file, target_size=(224, 224))
+    img_array = keras.utils.img_to_array(img_array, dtype='float32')
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+
+def get_predictions(predictions: np.array('float')):
+    # print(np.argmax(predictions), get_labels())
+    font_name = get_labels()[np.argmax(predictions)]
+    accuracy = predictions[np.argmax(predictions)]
+    return font_name + " " + str(round(accuracy * 100, 2)) + "%"
+
+
+def get_labels():
+    labels = [name for name in os.listdir("./test_data/") if os.path.isdir(os.path.join("./test_data/", name))]
+    return sorted(labels)
+
+
+def main(files_to_match: [], model):
+    for img in files_to_match:
+        img_batch = load_image(img)
+        prediction = predict_from_image(img_batch, model)
+        print("Raw prediction numbers", prediction[0])
+        print(get_predictions(prediction[0]))
+
+
+def load_model():
+    model_file = 'EfficientNetV2B1_model'
+    model = keras.models.load_model(model_file)
+    return model
+
+if __name__ == '__main__':
+    user_file_lst = ['user_files/test_img.jpg', 'user_files/Ancient_times.jpg', 'user_files/Ancient_cour.jpg']
+    main(user_file_lst, load_model())
